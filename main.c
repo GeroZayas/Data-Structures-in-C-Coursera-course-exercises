971b79018efa3db661ad42aee026c7e50ac8e42d
Gero Zayas <gerozayas@gmail.com>
2026-09-12 23:27:47 +0200

    update

--- main.c
+++ main.c
@@ -3,19 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 
-void calcpay(float *p, float r, float h) {
-  if (h > 40) {
-    int diff;
-    diff = h - 40;
-    printf("diff is %i\n", diff);
-    *p = (r * 40) + diff * (r * 1.5);
-  } else {
-    *p = r * h;
-  }
-  // printf("%f\n", *p);
-}
-
 int main(void) {
-  float pay;
-  calcpay(&pay, 5.25, 10);
+  char in_one[50];
+  char in_two[50];
+  char final_str[100];
+  puts("Enter two strings");
+  scanf("%s", in_one);
+  scanf("%s", in_two);
+  strcpy(final_str, in_one);
+  strcat(final_str, " & ");
+  strcat(final_str, in_two);
+  // puts(final_str);
 }
