3e400c85dbc9bd7222a647e4dbe58665b81cb8ff
Gero Zayas <gerozayas@gmail.com>
2026-09-13 13:33:57 +0200

    update

--- main.c
+++ main.c
@@ -1,24 +1,8 @@
+#include "libs/Forge/forge.h"
 #include "stuff.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
-void py_rstrip(char inp[]) {
-  int count = 0;
-  int original_length = strlen(inp);
-  for (int i = original_length - 1; i >= 0; i--) {
-    if (inp[i] == ' ') {
-      count += 1;
-      printf("count = %d\n", count);
-    } else {
-      break;
-    }
-    printf("%c\n", inp[i]);
-  }
-}
-
 int main(void) {
-  char s1[] = "   Hello   World    ";
-  py_rstrip(s1);
-  printf("-%s-\n", s1);
 }
