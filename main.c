ed090ca4a9aa4a8fcd6267201da40d13c2e40815
Gero Zayas <gerozayas@gmail.com>
2026-09-13 13:42:32 +0200

    update

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+
 
 int main(void) {
+  float a[3] = {3.5f, 4.5f, 5.5f};
+  float length = f_length3f(a);
+  printf("LENGTH of a = {3.5f, 4.5f, 5.5f} IS %f\n", length);
+  printf("Forge: %f\n", f_length3f(a));
+  printf("C:     %f\n", sqrtf(61.25f));
 }
