81769f42c40749fc7aaaaa0eae96e0f52b2626f5
Gero Zayas <gerozayas@gmail.com>
2026-09-13 20:35:50 +0200

    update

--- main.c
+++ main.c
@@ -12,4 +12,11 @@ int main(void) {
   printf("LENGTH of a = {3.5f, 4.5f, 5.5f} IS %f\n", length);
   printf("Forge: %f\n", f_length3f(a));
   printf("C:     %f\n", sqrtf(61.25f));
+
+  float dist;
+  float *one, *two;
+  one = &a[0]; two = &a[1]; 
+  dist = f_distance2f(one, two);
+  printf("dist:  %f\n", dist);
+
 }
