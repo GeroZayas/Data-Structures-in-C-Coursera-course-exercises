940398151b63f7ff602ad1449be47a6e79adcd12
Gero Zayas <gerozayas@gmail.com>
2026-09-14 08:44:35 +0200

    update

--- main.c
+++ main.c
@@ -3,29 +3,36 @@
 #include <stdio.h>
 #include <string.h>
 
-void py_lstrip(char inp[]) {
-  int original_length = strlen(inp);
-  int i;
-  int count = 0;
-  for (i = 0; i < original_length; i++) {
-    if (inp[i] == ' ') {
-      count++;
-      continue;
-    } else {
-      break;
-    }
-  }
-  int diff = original_length - count;
-  char temp[diff];
-  for (i = 0; i < diff; i++){
-    temp[i] = inp[i + count];
-  }
-  temp[diff] = '\0';
-  strcpy(inp, temp);  
-}
+/*
+ The program will create a 10 element array and read in 10 integers into the array.
+ Then the program will print the integers backwards.
+ Then the program will scan for entries in the array which contain the value 100 and print out the
+ index of the entries with the number 100. T he program will also count the number of entries which
+ equal 100. The program should work even if there are no entries which equal 100. See the sample
+ output for the expect format of the output.
+ */
 
 int main(void) {
-  char s1[] = "   Hello   World    ";
-  py_lstrip(s1);
-  printf("-%s-\n", s1);
+  int i, v, arr[10];
+  int hundred_counter = 0;
+  for (i = 0; i < 10; i++) {
+    scanf("%d", &v);
+    arr[i] = v;
+    if (v == 100) {
+      hundred_counter++;
+    }
+  }
+
+  for (i = 10 - 1; i >= 0; i--) {
+    printf("numb[%d] = %d\n", i, arr[i]);
+  }
+  puts("");
+  puts("Searching for entries equal to 100\n");
+  for (i = 0; i < 10; i++) {
+    if (arr[i] == 100) {
+      printf("Found 100 at %d\n", i);
+    }
+  }
+  puts("");
+  printf("Found %d entries with 100\n", hundred_counter);
 }
