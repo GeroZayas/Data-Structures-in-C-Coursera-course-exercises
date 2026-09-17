2e03069830dc363ce15e941c134c9c2f010391cf
Gero Zayas <gerozayas@gmail.com>
2026-09-17 08:15:01 +0200

    update

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "stuff.h"
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -17,7 +18,22 @@ void print_person(Person *person) {
 }
 
 int main(void) {
-    Person gero = {"Gero", 35, "Barcelona City"};
+    Person    gero  = {"Gero", 35, "Barcelona City"};
+    Person    mar   = {"Mar", 35, "L'Example"};
+    Person    elisa = {"Elisa", 60, "Holguin"};
+    Person    people[3];
+    size_t    len_array;
+    short int i;
+
     print_person(&gero);
+    people[0] = gero;
+    people[1] = mar;
+    people[2] = elisa;
+
+    len_array = sizeof(people) / sizeof(people[0]);
+    for (i = 0; i < len_array; i++) {
+        print_person(&people[i]);
+    }
+
     return 0;
 }
