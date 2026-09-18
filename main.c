9b47feceeaac37cedee87a6157bdf9068b6b36d9
Gero Zayas <gerozayas@gmail.com>
2026-09-18 08:42:46 +0200

    update

--- main.c
+++ main.c
@@ -1,50 +1,59 @@
 #include "stuff.h"
 #include <stddef.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
 Structures - Linked List
 */
+#define MAXLINE 1000
 
-typedef struct {
-    char name[30];
-    int  age;
-    char adress[50];
-} Person;
-
-void print_person(Person *person) {
-    printf("PERSON => name=%s age=%d address=%s\n", person->name, person->age, person->adress);
-}
+typedef struct lnode {
+    char         *text;
+    struct lnode *prev;
+    struct lnode *next;
+} lnode;
 
 int main(void) {
-    Person    gero  = {"Gero", 35, "Barcelona City"};
-    Person    mar   = {"Mar", 35, "L'Example"};
-    Person    elisa = {"Elisa", 60, "Holguin"};
-    Person    people[3];
-    size_t    len_array;
-    short int i;
+    lnode *head = NULL;
+    lnode *tail = NULL;
+    lnode *current;
+    char   line[MAXLINE];
 
-    people[0] = gero;
-    people[1] = mar;
-    people[2] = elisa;
+    while (fgets(line, MAXLINE, stdin) != NULL) {
+        char *save = (char *)malloc(strlen(line) + 1); // void * -> char *
+        strcpy(save, line);
 
-    len_array = sizeof(people) / sizeof(people[0]);
-    for (i = 0; i < len_array; i++) {
-        print_person(&people[i]);
+        lnode *new = (lnode *)malloc(sizeof(lnode)); // void * -> lnode *
+        new->text  = save;
+        new->next  = NULL;
+        new->prev  = tail;
+
+        if (tail != NULL) {
+            tail->next = new;
+        }
+
+        tail = new;
     }
 
     return 0;
 }
 
+/*
+ |
+`'´
+tail NULL
+new.next = NULL
+new.prev = NULL
 
+tail = new
+----
+new2.next = NULL
+new2.prev = new
 
+new.next = new2
 
-
-
-
-
-
-
-
+tail = new2
+----
+*/
