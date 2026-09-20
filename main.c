579177f7c77ea0e2d6229ac25f15c5a301cc3ac8
Gero Zayas <gerozayas@gmail.com>
2026-09-20 08:59:26 +0200

    update

--- main.c
+++ main.c
@@ -8,33 +8,8 @@
 Structures - Linked List
 */
 
-typedef struct {
-    char name[30];
-    int  age;
-    char adress[50];
-} Person;
-
-void print_person(Person *person) {
-    printf("PERSON => name=%s age=%d address=%s\n", person->name, person->age, person->adress);
-}
 
 int main(void) {
-    Person    gero  = {"Gero", 35, "Barcelona City"};
-    Person    mar   = {"Mar", 35, "L'Example"};
-    Person    elisa = {"Elisa", 60, "Holguin"};
-    Person    people[3];
-    size_t    len_array;
-    short int i;
-
-    people[0] = gero;
-    people[1] = mar;
-    people[2] = elisa;
-
-    len_array = sizeof(people) / sizeof(people[0]);
-    for (i = 0; i < len_array; i++) {
-        print_person(&people[i]);
-    }
-
     return 0;
 }
 
